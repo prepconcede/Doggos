d8890b11f3fc65eb384ac7ac02e049508bb64267
Ivan Kruglov <ivankruglovv@gmail.com>
2020-05-02 00:59:18 +0300

    shortened MainView code with for loops

--- src/main/kotlin/DogController.kt
+++ src/main/kotlin/DogController.kt
@@ -1,12 +1,9 @@
 import com.google.gson.Gson
-import com.google.gson.annotations.SerializedName
 import tornadofx.Controller
 import java.net.URL
 
 data class Response(
-    @SerializedName("message")
     var message: List<String>? = null,
-    @SerializedName("status")
     var status: String? = null
 )
 
@@ -14,7 +11,6 @@ class DogController : Controller() {
     fun getData(num: Int): List<String> {
         val json = URL("https://dog.ceo/api/breeds/image/random/$num").readText()
         val parsedJson = Gson().fromJson(json, Response::class.java)
-        val msgs = parsedJson?.message ?: emptyList()
-        return msgs
+        return parsedJson?.message ?: emptyList()
     }
 }
--- src/main/kotlin/MainView.kt
+++ src/main/kotlin/MainView.kt
@@ -1,57 +1,26 @@
 import tornadofx.*
 
 class MainView : View("My View") {
-    val controller: DogController by inject()
-    var images = controller.getData(9)
+    private val controller: DogController by inject()
+    private var images = controller.getData(9).asObservable()
+    private var nums = listOf(listOf(0, 1, 2), listOf(3, 4, 5), listOf(6, 7, 8))
     override val root = borderpane {
         center {
             gridpane {
-                row {
-                    imageview(images[0]) {
-                        fitWidth = 300.0
-                        fitHeight = 200.0
-                    }
-                    imageview(images[1]) {
-                        fitWidth = 300.0
-                        fitHeight = 200.0
-                    }
-                    imageview(images[2]) {
-                        fitWidth = 300.0
-                        fitHeight = 200.0
-                    }
-                }
-                row {
-                    imageview(images[3]) {
-                        fitWidth = 300.0
-                        fitHeight = 200.0
-                    }
-                    imageview(images[4]) {
-                        fitWidth = 300.0
-                        fitHeight = 200.0
-                    }
-                    imageview(images[5]) {
-                        fitWidth = 300.0
-                        fitHeight = 200.0
-                    }
-                }
-                row {
-                    imageview(images[6]) {
-                        fitWidth = 300.0
-                        fitHeight = 200.0
-                    }
-                    imageview(images[7]) {
-                        fitWidth = 300.0
-                        fitHeight = 200.0
-                    }
-                    imageview(images[8]) {
-                        fitWidth = 300.0
-                        fitHeight = 200.0
+                for (i in 0..2) {
+                    row {
+                        for (j in 0..2) {
+                            imageview(images[nums[i][j]]) {
+                                fitWidth = 300.0
+                                fitHeight = 200.0
+                            }
+                        }
                     }
                 }
                 row {
                     button("vibe check") {
                         action {
-                            images = controller.getData(9)
+                            images = controller.getData(9).asObservable()
                         }
                     }
                 }
